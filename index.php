ab91b2ac3a7bdebb330520e2d59e1e295d5c7c4b
Nate M <Mr.awsmness@gmail.com>
2015-04-22 11:22:45 -0700

    (Milestone 5 pushed with last push + milestone 4) milestone 6

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 	<li rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<div class="wrap">
-	
-	</div>
+	<div class="task-list">
+			<ul>
+				<?php require("includes/connect.php"); ?>
+			</ul>
+		</div>
+		<form class="add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
 </body>
 </html>
