0941583c32eb6d5f71fe9df96ad0879eb6fb628b
Nate M <Mr.awsmness@gmail.com>
2015-04-22 11:18:02 -0700

    Milestone 4

---
+++ includes/connect.php
@@ -0,0 +1,16 @@
+<?php
+	//connects it to localhost, parsing in username and password to access the database "todo2"
+	$mysqli = new mysqli("localhost", "root", "root", "todo2");
+
+	//if the connection doesn't work, kill the connection and give a message
+	if($mysqli->connect_error){
+		die("connect error(" . $mysqli->connect_errno . ")"
+			. $mysqli->connect_error);
+	}
+	else{
+		echo "connection made";
+	}
+	
+	$mysqli->close();
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>To-Do 2</title>
+	<li rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
