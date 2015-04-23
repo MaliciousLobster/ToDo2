611bcd80920347e3b692f4301fb91cdae1d30b36
Nate M <Mr.awsmness@gmail.com>
2015-04-23 10:52:59 -0700

    milestone 7

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>To-Do 2</title>
-	<li rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css"/>
 </head>
 <body>
 	<div class="task-list">
